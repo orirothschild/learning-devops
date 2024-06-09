c51049e62ca9f11501d7e4fedd6bb81ce639f9e7
ori rothschild <ori.rothschild@deepinstinct.com>
2024-06-09 16:44:29 +0300

    add

--- increment-app/bash.sh
+++ increment-app/bash.sh
@@ -1,5 +1,5 @@
 
-docker build  --platform linux/amd64       -t basic:latest .
-docker tag basic:latest gcr.io/di-services/testimages/basic:latest
+docker build  --platform linux/amd64       -t incrementer:latest .
+docker tag incrementer:latest gcr.io/di-services/testimages/incrementer:latest
 
-docker push gcr.io/di-services/testimages/basic:latest
+docker push gcr.io/di-services/testimages/incrementer:latest
